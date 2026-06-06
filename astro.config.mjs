7680e75c42646859ec326ceb9e36075516955a4f
athulthomasatz <thomasathul089@gmail.com>
2026-06-06 22:08:03 +0530

    fix: add @astrojs/tailwind integration to restore styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://athulthomasatz.github.io',
-  integrations: [react(), sitemap()],
+  integrations: [react(), tailwind(), sitemap()],
 });
